ce9a8549e96b50694bfbcd9e1144c3ec5899c54f
Devansh Goel <Devansh@MacBook-Pro-8.local>
2025-06-15 19:39:05 -0700

    help

---
+++ calculator.lua
@@ -0,0 +1,34 @@
+print("First Number: ")
+local x = io.read()
+
+print("Input Operation: ")
+local op = io.read()
+
+print("Second Number: ")
+local y = io.read()
+
+if op == "+" then
+	print(x+y)
+end
+
+if op == "-" then
+        print(x-y)
+end
+
+if op == "*" then
+        print(x*y)
+end
+
+if op == "/" then
+        print(x/y)
+end
+
+if op == "%" then
+        print(x%y)
+end
+
+if op == "^" then
+        print(x^y)
+end
+
+
